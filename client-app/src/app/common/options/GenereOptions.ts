bb7ba7f4247b7d18944e0db81ee7a3b2561a705a
Meron Leshem <MeronLeshem10@gmail.com>
2022-12-16 14:14:16 +0200

    Add Formik and form validations

---
+++ client-app/src/app/common/options/GenereOptions.ts
@@ -0,0 +1,7 @@
+export const genereOptions = [
+    {text: 'Thriller', value: 'thriller' },
+    {text: 'Fantasy', value: 'fantasy' },
+    {text: 'Drama', value: 'drama' },
+    {text: 'Self-help', value: 'self-help' },
+    {text: 'Kids', value: 'kids' }
+]
--- client-app/src/features/books/forms/BookForm.tsx
+++ client-app/src/features/books/forms/BookForm.tsx
@@ -1,27 +1,41 @@
 import { observer } from "mobx-react-lite";
 import React, { ChangeEvent, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Button, Form, Segment } from 'semantic-ui-react';
+import { Button, FormField, Header, Label, Segment } from 'semantic-ui-react';
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { Book } from "../../../app/models/book";
 import { useStore } from "../../../app/stores/store";
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
+import { Formik, Form, ErrorMessage } from "formik";
+import * as Yup from 'yup';
+import ValidateTextInput from "../../../app/common/form/ValidateTextInput";
+import ValidateSelectInput from "../../../app/common/form/ValidateSelectInput";
+import { genereOptions } from "../../../app/common/options/GenereOptions";
 
 export default observer(function BookForm() {
     const { bookStore } = useStore();
     const { createBook, updateBook, loadBook, loadingInitial } = bookStore;
     const { id } = useParams();
     const navigate = useNavigate()
-    
+
 
     const [book, setBook] = useState<Book>({
         id: '',
         title: '',
         author: '',
-        year: 0,
+        year: 2022,
         genere: '',
         availableQuantity: 0,
         image: ''
+    });
+
+    const validationSchema = Yup.object({
+        title: Yup.string().required('The book title is required'),
+        author: Yup.string().required('The author is required'),
+        year: Yup.number().required().min(1800),
+        genere: Yup.string().required(),
+        availableQuantity: Yup.number().required().min(0),
+        image: Yup.string().required()
     })
 
     useEffect(() => {
@@ -29,8 +43,8 @@ export default observer(function BookForm() {
             loadBook(id).then(book => setBook(book!));
     }, [id, loadBook]);
 
-    function handleSubmit() {
-        if(!book.id){
+    function handleFormSubmit(book: Book) {
+        if (!book.id) {
             book.id = uuid();
             createBook(book).then(() => navigate(`/books/${book.id}`))
         } else {
@@ -38,26 +52,32 @@ export default observer(function BookForm() {
         }
     }
 
-    function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-        const { name, value } = event.target;
-        setBook({ ...book, [name]: value });
-    }
-
     if (loadingInitial)
         return <LoadingComponent />
 
     return (
         <Segment clearing>
-            <Form onSubmit={handleSubmit} autoComplete='off'>
-                <Form.Input placeholder='Title' value={book.title} name='title' onChange={handleInputChange} />
-                <Form.Input placeholder='Author' value={book.author} name='author' onChange={handleInputChange} />
-                <Form.Input placeholder='Year' value={book.year} name='year' onChange={handleInputChange} />
-                <Form.Input placeholder='Genere' value={book.genere} name='genere' onChange={handleInputChange} />
-                <Form.Input placeholder='Quantity' value={book.availableQuantity} name='availableQuantity' onChange={handleInputChange} />
-                <Form.Input placeholder='Image' value={book.image} name='image' onChange={handleInputChange} />
-                <Button floated="right" positive type='submit' content='Submit' />
-                <Button as={Link} to={'/books'}  floated="right" type='button' content='Cancel' />
-            </Form>
+            <Header content='Book Details' sub color="blue" />
+            <Formik
+                validationSchema={validationSchema}
+                enableReinitialize
+                initialValues={book}
+                onSubmit={values => handleFormSubmit(values)}>
+                {({ handleSubmit, isValid, isSubmitting, dirty }) => (
+                    <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                        <ValidateTextInput name='title' placeholder="Title" />
+                        <ValidateTextInput placeholder='Author' name='author' />
+                        <ValidateTextInput placeholder='Year' name='year' />
+                        <ValidateSelectInput options={genereOptions} placeholder='Genere' name='genere' />
+                        <ValidateTextInput placeholder='Quantity' name='availableQuantity' />
+                        <ValidateTextInput placeholder='Image' name='image' />
+                        <Button
+                            disabled={isSubmitting || !dirty || !isValid }
+                            floated="right" positive type='submit' content='Submit' />
+                        <Button as={Link} to={'/books'} floated="right" type='button' content='Cancel' />
+                    </Form>
+                )}
+            </Formik>
         </Segment>
     )
 })
